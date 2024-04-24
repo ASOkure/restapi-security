f7042fa4b4df5f258d6302d7288f259370e07318
Akanimo Sam Okure <akan.okure@gmail.com>
2024-04-24 16:28:41 +0100

    project set up

---
+++ src/main/java/com/aso/springbootrest/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.aso.springbootrest.dao;
+
+import com.aso.springbootrest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+}
